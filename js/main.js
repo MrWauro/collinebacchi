8457dfe7b987f8131f4fc057eb61eeab259895f2
Mr Wauro <101128446+MrWauro@users.noreply.github.com>
2026-02-11 12:17:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-const elements = document.querySelectorAll('.feature, .banner');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
-    }
-  });
-}, { threshold: 0.2 });
-
-elements.forEach(el => {
-  el.style.opacity = 0;
-  el.style.transform = "translateY(40px)";
-  el.style.transition = "all 0.9s ease";
-  observer.observe(el);
-});
-
 document.addEventListener("DOMContentLoaded", function() {
   const toggle = document.getElementById("menu-toggle");
-  const nav = document.querySelector("nav ul");
+  const navMenu = document.getElementById("nav-menu");
 
-  toggle.addEventListener("click", function() {
-    nav.classList.toggle("active");
+  if (toggle && navMenu) {
+    toggle.addEventListener("click", function() {
+      navMenu.classList.toggle("active");
+    });
+  }
+});
+
+const links = navMenu.querySelectorAll("a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
   });
 });
