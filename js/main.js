8230ded298ff6efa57b061f1d0c1c0c7abcfa7f1
Mr Wauro <101128446+MrWauro@users.noreply.github.com>
2026-02-11 12:52:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+
   const toggle = document.getElementById("menu-toggle");
   const navMenu = document.getElementById("nav-menu");
 
   if (toggle && navMenu) {
+
     toggle.addEventListener("click", function() {
       navMenu.classList.toggle("active");
     });
+
+    // Chiude menu quando clicchi un link
+    const links = navMenu.querySelectorAll("a");
+
+    links.forEach(link => {
+      link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+      });
+    });
   }
-});
 
-const links = navMenu.querySelectorAll("a");
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    navMenu.classList.remove("active");
-  });
 });
